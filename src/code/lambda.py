05913fd1a83858c06fab6103b0926f41cb440885
Pedro Haramoto <pedroharamoto@gmail.com>
2023-11-04 20:01:20 -0300

    logs

--- src/code/lambda.py
+++ src/code/lambda.py
@@ -6,33 +6,40 @@ import os
 def main(event, context):
     try:        
         # Create a Secrets Manager client
-        print("chegou")
         secret_name = "rds/fastfood/secret"
         session = boto3.session.Session()
         client = session.client(
             service_name='secretsmanager',
             region_name='us-east-1'
         )
-        print("chegou2")
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
         secret = json.loads(get_secret_value_response['SecretString'])
-        username = secret['username']
-        password = secret['password']
-        print('username: ' + username)
+        db_username = secret['username']
+        db_password = secret['password']
+        db_name = secret['dbname']
+        db_host = secret['host']
 
-        #DB connection
-        db_host = os.environ['RDS_ENDPOINT']
-        db_name = os.environ['DB_NAME']
-        print("DB NAME: "+os.environ.get('DB_NAME'))
+        #DB conn
 
         connection = psycopg2.connect(
             host=db_host,
             database=db_name,
-            user=username,
-            password=password
+            user=db_username,
+            password=db_password
         )
         cursor = connection.cursor()
         print("Connected to the database")
+        
+        cursor.close()
+        connection.close()
+        print("Database connection closed") 
+
+        #Response        
+        response = {
+            'statusCode': 200,
+            'username': username
+        }
+        return response
     except Exception as error:
         print("Error connecting to the database:", error)
         response = {
@@ -40,14 +47,3 @@ def main(event, context):
             'msg': 'Error'
         }
         return response
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print("Database connection closed")            
-            #Response        
-            response = {
-                'statusCode': 200,
-                'username': username
-            }
-            return response
